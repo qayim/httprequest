16352ee9ef1fbeecd02f0e3816474a8112d2de7d
Qayim <200919@student.upm.edu.my>
2023-07-04 17:08:03 +0800

    1st commit

---
+++ screens/RecentExpenses.js
@@ -0,0 +1,61 @@
+import { useContext, useEffect, useState } from "react";
+
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
+import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import ErrorOverlay from "../components/UI/ErrorOverlay";
+
+function RecentExpenses() {
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+  const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      setIsFetching(true);
+      //try catch in case there is an error
+      try {
+        const expenses = await fetchExpenses();
+        expensesCtx.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses");
+      }
+      //change state to stop loading when done fetching
+      setIsFetching(false);
+    }
+    getExpenses();
+  }, []);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return expense.date >= date7DaysAgo && expense.date <= today;
+  });
+
+  //function to make error empty to disable error overlay
+  function errorHandler(){
+    setError(null);
+  }
+
+  //check if error has contents -> error component triggered
+  if(error && !isFetching){
+    return <ErrorOverlay message={error} onConfirm={errorHandler}/>
+  }
+  //check if loading finished or not
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
+
+  return (
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
+  );
+}
+
+export default RecentExpenses;
